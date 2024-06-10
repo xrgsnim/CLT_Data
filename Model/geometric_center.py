50ca84166025737a1db1bb3fa93408032c1c315b
Jeonghwa Lee <hwa8558@pukyong.ac.kr>
2024-06-10 22:31:57 +0900

    automate get geometric center

--- Model/geometric_center.py
+++ Model/geometric_center.py
@@ -1,21 +1,4 @@
 import numpy as np
-import get_grid
-
-locations = [(1,0), (1,1), (2,0), (1,2), (2,1), (3,0), (1,3), (2,2), (3,1), (4,0), (1,4), (2,3), (3,2), (4,1), (5,0),
-             (2,4), (3,3), (4,2), (5,1), (6,0), (3,4), (4,3), (5,2), (6,1), (4,4), (5,3), (6,2), (5,4), (6,3), (6,4)]
-
-def create_grid(stack_num, height_num, _levels, _locations):
-    grid = np.zeros((height_num, stack_num))
-    sorted_levels = sorted(_levels)
-    
-    for i in range(len(sorted_levels)):
-        column_idx = _locations[i][0] - 1
-        row_idx = height_num - _locations[i][1] - 1 
-        
-        grid[row_idx][column_idx] = sorted_levels[i]
-    
-    return grid
-        
     
 def div_level(_weights, _level_num):
     w_min = min(_weights)
@@ -36,8 +19,8 @@ def get_level(_weights, _level_range):
                 _container_level.append(level_idx)
     return _container_level  
 
-
-def set_geometric_grid(stack_num, height_num, levels):
+# 오른쪽 -> 왼쪽으로 숫자 채우기
+# def set_geometric_grid(stack_num, height_num, levels):
     grid = np.zeros((height_num, stack_num))
     
     # sort weights in increasing order
@@ -110,6 +93,48 @@ def set_geometric_grid(stack_num, height_num, levels):
     print('geometric grid by level \n', grid, '\n ------------------- \n')
     return grid
 
+# 왼쪽에서 오른쪽으로 숫자 채우기
+def set_geometric_grid(_stack_idx, _height_idx, min_stack_idx, max_stack_idx, min_height_idx, max_height_idx, sorted_levels, _grid, _set_container_num):
+        
+    # Set the next location
+    if _set_container_num < len(sorted_levels):    
+        
+        _grid[_height_idx][_stack_idx] = sorted_levels[_set_container_num]
+        _set_container_num += 1
+        
+        if _stack_idx == max_stack_idx:
+            if min_height_idx != 0:
+                min_height_idx -= 1
+
+            #(0, 0) is full
+            else:
+                min_stack_idx += 1
+                
+            # stack_idx is max
+            if max_stack_idx == _grid.shape[1] - 1:
+                max_height_idx -= 1
+
+            else:
+                print('max_stack_idx is not max')       
+                max_stack_idx += 1
+                
+ 
+            _height_idx = min_height_idx
+            _stack_idx = min_stack_idx
+            
+        else:
+            if _stack_idx != max_stack_idx:
+                _stack_idx += 1
+                _height_idx += 1
+        
+        # print(grid, '\n')
+        set_geometric_grid(_stack_idx, _height_idx, min_stack_idx, max_stack_idx, min_height_idx, max_height_idx, sorted_levels, _grid, _set_container_num)
+    
+    else:
+        print('\nAll containers are placed')
+        # print('geometric grid by level \n', _grid, '\n ------------------- \n')
+
+    return _grid
 
 # get dictionary of geometric center
 def get_geometric_dict(_grid):
@@ -130,7 +155,7 @@ def get_geometric_dict(_grid):
                 # _dict[_grid[_h][_s]].append((_h +1, _s +1))
                 _dict[_grid[_h][_s]].append(location)
                 
-    print('best locations \n', _dict, '\n')
+    # print('best locations \n', _dict, '\n')
     return _dict
 
 
@@ -141,10 +166,26 @@ def get_geometric_center(_m, _h, _weights, _level_num):
     print('level range : ', level_range, '\n')
     
     container_level = get_level(_weights, level_range)
-    # geometric_grid = set_geometric_grid(_m, _h, container_level)
-    geometric_grid = create_grid(_m, _h, container_level, locations)
-    # geometric_grid = get_grid.place_containers_diagonally(container_level, _h, _m)
-    print('geometric grid : \n', geometric_grid, '\n')
+    
+    grid = np.zeros((_h, _m))
+    
+    # sort weights in increasing order
+    sorted_levels = sorted(container_level)
+    
+    # Height_idx : 0 ~ 4
+    height_idx = _h - 1
+    stack_idx = 0
+    stack_max_idx = stack_idx
+    stack_min_idx = stack_idx
+    height_max_idx = height_idx
+    height_min_idx = height_idx
+    set_container_num = 0
+    
+    
+    geometric_grid = set_geometric_grid(stack_idx, height_idx, stack_min_idx, stack_max_idx, height_min_idx, height_max_idx, sorted_levels, grid, set_container_num)
+    print('geometric grid by level \n', geometric_grid, '\n ------------------- \n')
+
+    
     geometric_dict = get_geometric_dict(geometric_grid)
 
     _geometric_center_dict = {}
@@ -168,14 +209,25 @@ def get_geometric_center(_m, _h, _weights, _level_num):
     return _geometric_center_dict
 
 
+
+
+
+# level_num = 9
 # # Number of stacks
 # m = 6
 
 # # Capacity of tiers
 # h = 5
 
-# w = np.arange(1, 16)
-# level_num = 9
+# w = np.arange(1, 31)
 
-# get_geometric_center(m, h, w, level_num)
+# height_idx = h - 1
+# stack_idx = 0
+# stack_max_idx = stack_idx
+# stack_min_idx = stack_idx
+# height_max_idx = height_idx
+# height_min_idx = height_idx
+# set_container_num = 0
+# grid = np.zeros((h, m))
 
+# geometric_grid = set_geometric_grid(stack_idx, height_idx, stack_min_idx, stack_max_idx, height_min_idx, height_max_idx, w, grid, set_container_num)
