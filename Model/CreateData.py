1a5daa0af18f9d0f671ae080ff1215a93e801126
Jeonghwa Lee <90810455+ppurify@users.noreply.github.com>
2024-06-14 16:49:50 +0900

    Add Input Data

--- Model/CreateData.py
+++ Model/CreateData.py
@@ -13,16 +13,16 @@ import random
 # new_con_num_list = [10, 15, 20]
 #--------------------------------------------
 # repeat time
-repeat_num = 15
+repeat_num = 30
 
 # initial_con_num_list = [5, 10, 15]
 # new_con_num_list = [10, 15, 20]
 
-initial_con_num_list = [0, 3, 5, 7]
-new_con_num_list = [10, 13, 15, 20]
+initial_con_num_list = [0, 5, 10, 15, 20]
+new_con_num_list = [50, 45, 40, 35, 30]
 
-stack_num = 6
-tier_num = 5
+stack_num = 10
+tier_num = 6
 initial_con_start_idx = 1
 
 def get_priority(_priority_list, container_num):
@@ -47,40 +47,75 @@ def get_random_data(repeat_num, initial_con_num_list, new_con_num_list, stack_nu
     if len(initial_con_num_list) ==  0 or len(new_con_num_list) == 0:
         print('Error : Check initial_con_num or new_con_num_list')
     else:    
-        for initial_con_num in initial_con_num_list:
-            for new_con_num in new_con_num_list:
+        for container_num_idx in range(len(initial_con_num_list)):
+            
+            initial_con_num = initial_con_num_list[container_num_idx]
+            new_con_num = new_con_num_list[container_num_idx]
+            
+            new_con_start_idx = initial_container_start_idx + initial_con_num
+            folderPath = 'Data/Initial_' + str(initial_con_num) + '/New_' + str(new_con_num) + '/Input'
+            print('Folder Path : ', folderPath, '\n')
+            
+            # Check exist folder
+            if not os.path.exists(folderPath):
+                os.makedirs(folderPath)
+                print('Create Folder : ', folderPath, '\n')
+    
+            for repeat_num_idx in range(repeat_num):
                 
-                new_con_start_idx = initial_container_start_idx + initial_con_num
-                folderPath = 'Data/Initial_' + str(initial_con_num) + '/New_' + str(new_con_num) + '/Input'
-                print('Folder Path : ', folderPath, '\n')
+                initial_container_name = 'Initial_state_ex' + str(repeat_num_idx + 1)
+                new_container_name = 'Container_ex' + str(repeat_num_idx + 1)
                 
-                # Check exist folder
-                if not os.path.exists(folderPath):
-                    os.makedirs(folderPath)
-                    print('Create Folder : ', folderPath, '\n')
-        
-                for repeat_num_idx in range(repeat_num):
-                    
-                    initial_container_name = 'Initial_state_ex' + str(repeat_num_idx + 1)
-                    new_container_name = 'Container_ex' + str(repeat_num_idx + 1)
-                    
-                    print('--------- Start Create Input Data ---------')
-                    print('Initial Container Number : ', initial_con_num, '\nNew Container Number : ', new_con_num)
+                print('--------- Start Create Input Data ---------')
+                print('Initial Container Number : ', initial_con_num, '\nNew Container Number : ', new_con_num)
 
-                    # group_num = 3
-                    # 0 ~ group_num
-                    # priority_list = [i for i in range(0, group_num+1)]
+                # group_num = 3
+                # 0 ~ group_num
+                # priority_list = [i for i in range(0, group_num+1)]
+                                    
+                # initial_con_priority = get_priority(priority_list, initial_con_num)
+                initial_con_priority = [0 for _ in range(initial_con_num)]
+                
+                # Save Input Data for Initial Container
+                saveCSV.InitialContainerCSV(folderPath, initial_container_name, initial_container_start_idx, initial_con_num, stack_num, tier_num, initial_con_priority)
+                
+                # new_con_priority = get_priority(priority_list, new_con_num)
+                new_con_priority = [0 for _ in range(new_con_num)]
+                # Save Input Data for New Container
+                saveCSV.NewContainerCSV(folderPath, new_container_name, new_con_start_idx, new_con_num, new_con_priority)   
+            # for new_con_num in new_con_num_list:
+                
+            #     new_con_start_idx = initial_container_start_idx + initial_con_num
+            #     folderPath = 'Data/Initial_' + str(initial_con_num) + '/New_' + str(new_con_num) + '/Input'
+            #     print('Folder Path : ', folderPath, '\n')
+                
+            #     # Check exist folder
+            #     if not os.path.exists(folderPath):
+            #         os.makedirs(folderPath)
+            #         print('Create Folder : ', folderPath, '\n')
+        
+            #     for repeat_num_idx in range(repeat_num):
+                    
+            #         initial_container_name = 'Initial_state_ex' + str(repeat_num_idx + 1)
+            #         new_container_name = 'Container_ex' + str(repeat_num_idx + 1)
+                    
+            #         print('--------- Start Create Input Data ---------')
+            #         print('Initial Container Number : ', initial_con_num, '\nNew Container Number : ', new_con_num)
+
+            #         # group_num = 3
+            #         # 0 ~ group_num
+            #         # priority_list = [i for i in range(0, group_num+1)]
                                       
-                    # initial_con_priority = get_priority(priority_list, initial_con_num)
-                    initial_con_priority = [0 for _ in range(initial_con_num)]
+            #         # initial_con_priority = get_priority(priority_list, initial_con_num)
+            #         initial_con_priority = [0 for _ in range(initial_con_num)]
                     
-                    # Save Input Data for Initial Container
-                    saveCSV.InitialContainerCSV(folderPath, initial_container_name, initial_container_start_idx, initial_con_num, stack_num, tier_num, initial_con_priority)
+            #         # Save Input Data for Initial Container
+            #         saveCSV.InitialContainerCSV(folderPath, initial_container_name, initial_container_start_idx, initial_con_num, stack_num, tier_num, initial_con_priority)
                     
-                    # new_con_priority = get_priority(priority_list, new_con_num)
-                    new_con_priority = [0 for _ in range(new_con_num)]
-                    # Save Input Data for New Container
-                    saveCSV.NewContainerCSV(folderPath, new_container_name, new_con_start_idx, new_con_num, new_con_priority)
+            #         # new_con_priority = get_priority(priority_list, new_con_num)
+            #         new_con_priority = [0 for _ in range(new_con_num)]
+            #         # Save Input Data for New Container
+            #         saveCSV.NewContainerCSV(folderPath, new_container_name, new_con_start_idx, new_con_num, new_con_priority)
 
 
 get_random_data(repeat_num, initial_con_num_list, new_con_num_list, stack_num, tier_num, initial_con_start_idx)
