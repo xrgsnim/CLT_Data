627a0454687ade06c4732403ae4bf316d5a01bad
Jeonghwa Lee <hwa8558@pukyong.ac.kr>
2024-07-03 15:58:51 +0900

    add input data with group data

--- Model/CreateData.py
+++ Model/CreateData.py
@@ -5,37 +5,33 @@ import random
 
 stack_num = 6
 tier_num = 5
-container_num = 27
+container_num = 23
 repeat_num = 30
+group_num = 3
 
-initial_con_num_list = [0, 5, 7, 10, 15]
+# initial_con_num_list = [0, 3, 5, 7, 10, 15]
+initial_con_num_list = [3]
 new_con_num_list = []
 for _initial_num in initial_con_num_list:
     new_con_num_list.append(container_num - _initial_num)
 
 initial_con_start_idx = 1
 
-folder_name = f'Input_Data_{container_num}(stack_{stack_num}_tier_{tier_num})'
+folder_name = f'Ungrouped/Input_Data_{container_num}(stack_{stack_num}_tier_{tier_num})'
 
-def get_priority(_group_list, container_num):
+def get_group(_group_list, _container_num):
     
     container_group = []
     
-    for i in range(container_num):
+    for i in range(_container_num):
         # random choice in _priority_list
-        group = random.choice(_group_list)
-        
-        # if priority != 0:
-        #     # remove priority from _priority_list
-        #     _priority_list.remove(priority)
-        
+        group = random.choice(_group_list)        
         container_group.append(group)
-        
-    # return container_priority, _priority_list
+
     return container_group
        
 
-def get_random_data(repeat_num, initial_con_num_list, new_con_num_list, stack_num, tier_num, initial_container_start_idx):
+def get_random_data(repeat_num, initial_con_num_list, new_con_num_list, stack_num, tier_num, initial_container_start_idx, _group_num):
     if len(initial_con_num_list) ==  0 or len(new_con_num_list) == 0:
         print('Error : Check initial_con_num or new_con_num_list')
     else:    
@@ -62,54 +58,20 @@ def get_random_data(repeat_num, initial_con_num_list, new_con_num_list, stack_nu
                 print('--------- Start Create Input Data ---------')
                 print('Initial Container Number : ', initial_con_num, '\nNew Container Number : ', new_con_num)
 
-                # group_num = 3
                 # 0 ~ group_num
-                # priority_list = [i for i in range(0, group_num+1)]
+                group_list = [i for i in range(0, _group_num+1)]
                                     
-                # initial_con_priority = get_priority(priority_list, initial_con_num)
-                initial_con_group = [0 for _ in range(initial_con_num)]
+                initial_con_group = get_group(group_list, initial_con_num)
+                # initial_con_group = [0 for _ in range(initial_con_num)]
                 
                 # Save Input Data for Initial Container
                 saveCSV.InitialContainerCSV(folderPath, initial_container_name, repeat_num_idx, initial_container_start_idx, initial_con_num, stack_num, tier_num, initial_con_group)
                 
-                # new_con_priority = get_priority(priority_list, new_con_num)
-                new_con_group = [0 for _ in range(new_con_num)]
+                new_con_group = get_group(group_list, new_con_num)
+                # new_con_group = [0 for _ in range(new_con_num)]
+                
                 # Save Input Data for New Container
                 saveCSV.NewContainerCSV(folderPath, new_container_name, new_con_start_idx, new_con_num, new_con_group)   
-                
-            # for new_con_num in new_con_num_list:
-                
-            #     new_con_start_idx = initial_container_start_idx + initial_con_num
-            #     folderPath = 'Data/Initial_' + str(initial_con_num) + '/New_' + str(new_con_num) + '/Input'
-            #     print('Folder Path : ', folderPath, '\n')
-                
-            #     # Check exist folder
-            #     if not os.path.exists(folderPath):
-            #         os.makedirs(folderPath)
-            #         print('Create Folder : ', folderPath, '\n')
-        
-            #     for repeat_num_idx in range(repeat_num):
-                    
-            #         initial_container_name = 'Initial_state_ex' + str(repeat_num_idx + 1)
-            #         new_container_name = 'Container_ex' + str(repeat_num_idx + 1)
-                    
-            #         print('--------- Start Create Input Data ---------')
-            #         print('Initial Container Number : ', initial_con_num, '\nNew Container Number : ', new_con_num)
-
-            #         # group_num = 3
-            #         # 0 ~ group_num
-            #         # priority_list = [i for i in range(0, group_num+1)]
-                                      
-            #         # initial_con_priority = get_priority(priority_list, initial_con_num)
-            #         initial_con_priority = [0 for _ in range(initial_con_num)]
-                    
-            #         # Save Input Data for Initial Container
-            #         saveCSV.InitialContainerCSV(folderPath, initial_container_name, initial_container_start_idx, initial_con_num, stack_num, tier_num, initial_con_priority)
-                    
-            #         # new_con_priority = get_priority(priority_list, new_con_num)
-            #         new_con_priority = [0 for _ in range(new_con_num)]
-            #         # Save Input Data for New Container
-            #         saveCSV.NewContainerCSV(folderPath, new_container_name, new_con_start_idx, new_con_num, new_con_priority)
 
 
-get_random_data(repeat_num, initial_con_num_list, new_con_num_list, stack_num, tier_num, initial_con_start_idx)
+get_random_data(repeat_num, initial_con_num_list, new_con_num_list, stack_num, tier_num, initial_con_start_idx, group_num)
--- Model/saveCSV.py
+++ Model/saveCSV.py
@@ -104,7 +104,8 @@ def InitialContainerCSV(folderpath, fileName, ex_idx, start_idx, initial_contain
             
 
     with open(os.path.join(folderpath, fileName + '.csv'), 'w', newline='') as csvfile:
-        fieldnames = ['idx', 'loc_x', 'loc_y', 'loc_z', 'weight', 'group', 'emerg', 'size(ft)']
+        fieldnames = ['idx', 'loc_x', 'loc_y', 'loc_z', 'weight', 'group', 'size(ft)']
+        
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         
@@ -116,10 +117,9 @@ def InitialContainerCSV(folderpath, fileName, ex_idx, start_idx, initial_contain
             weight = weight_list[i]
             group = group_list[i]
             size = size_list[i]
-            emergency = 0
             
             # Write row to CSV file
-            writer.writerow({'idx': idx, 'loc_x': loc_x, 'loc_y': loc_y, 'loc_z': loc_z, 'weight': weight, 'group' : group, 'emerg' : emergency, 'size(ft)': size})
+            writer.writerow({'idx': idx, 'loc_x': loc_x, 'loc_y': loc_y, 'loc_z': loc_z, 'weight': weight, 'group' : group, 'size(ft)': size})
         
         print('--------- Success Create Input Data : ', fileName ,'---------\n')
 
@@ -132,15 +132,14 @@ def NewContainerCSV(folderpath, fileName, start_idx, new_container_num, group_li
     # random sequence : 1 ~ new_con_num 
     # sequence_list = random.sample(range(1, new_container_num + 1), new_container_num)
     sequence_list = np.arange(1, new_container_num + 1)
-    # emerg_list = get_emergency_list(new_container_num)
-    
+
     # random weight from 5.0 to 24.0 with up to 2 decimal places
     weight_list = [round(random.uniform(5.0, 24.0), 2) for i in range(new_container_num)]
     
     size_list = [20 for _ in range(new_container_num)]
     
     with open(os.path.join(folderpath, fileName + '.csv'), 'w', newline='') as csvfile:
-        fieldnames = ['idx', 'seq', 'group', 'emerg', 'weight', 'size(ft)']
+        fieldnames = ['idx', 'seq', 'group', 'weight', 'size(ft)']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         
@@ -150,17 +149,8 @@ def NewContainerCSV(folderpath, fileName, start_idx, new_container_num, group_li
             group = group_list[i]
             weight = weight_list[i]
             size = size_list[i]
-            # emergency = emerg_list[i]
-            emergency = 0
+        
             # Write row to CSV file
-            writer.writerow({'idx': idx, 'seq': seq, 'group': group, 'emerg' : emergency, 'weight': weight, 'size(ft)': size})
+            writer.writerow({'idx': idx, 'seq': seq, 'group': group, 'weight': weight, 'size(ft)': size})
         
         print('--------- Success Create Input Data : ', fileName ,'---------\n')
-
-# Get Emergency List
-def get_emergency_list(container_num):
-    
-    emergency_num = int(container_num * 0.1)
-    emergency_list = [1 for _ in range(emergency_num)] + [0 for _ in range(container_num - emergency_num)]
-    random.shuffle(emergency_list)
-    return emergency_list
